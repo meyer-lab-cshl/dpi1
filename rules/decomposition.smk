e271f3cf1e2bf90ba19888b973f128b048179895
HannahVMeyer <hannah.v.meyer@googlemail.com>
2020-04-22 21:20:44 -0400

    Fix lambda functions, fix typos in file names; adapt file ending

--- rules/decomposition.smk
+++ rules/decomposition.smk
@@ -1,16 +1,16 @@
-rule decompositions_short_stranded:
+rule decomposition_short_stranded:
     input:
         tc="{dir}/outPooled/tc.bed.gz",
         bw="{dir}/outPooled/ctssTotalCounts.{strand}.bw",
         genome=config["genome"]
     output:
-        bw="{dir}/outPooled/tc.short.{strand}.bed.gz",
+        bw="{dir}/outPooled/tc.short.{strand}.bed",
     params:
         length=config["decomposition"]["length"],
         count=config["decomposition"]["count"],
         ratio=config["decomposition"]["noise_subtraction_ratio"],
-        color=lambda wildcards: '255,0,0' if strand == "fwd" else '0,0,255',
-        sign=lambda wildcards: '+' if strand == "fwd" else '-'
+        color=lambda wildcards: '255,0,0' if wildcards.strand == "fwd" else '0,0,255',
+        sign=lambda wildcards: '+' if wildcards.strand == "fwd" else '-'
     shell:
         """
         scripts/decomposition_short.sh \
@@ -26,18 +26,18 @@ rule decompositions_short_stranded:
         """
 rule decomposition_short_merge:
     input:
-        fwd="{dir}/outPooled/tc.short.fwd.bed.gz",
-        rev="{dir}/outPooled/tc.short.rev.bed.gz",
+        fwd="{dir}/outPooled/tc.short.fwd.bed",
+        rev="{dir}/outPooled/tc.short.rev.bed",
     output:
         tc="{dir}/outPooled/tc.short.bed.gz"
     shell:
         """
-        gunzip -c {input.fwd} {input.rev} | \
+        cat {input.fwd} {input.rev} | \
         sort -k1,1 -k2,2n | \
         gzip -c > {output.tc}
         """
 
-rule decompositions_long:
+rule decomposition_long:
     input:
         tc="{dir}/outPooled/tc.bed.gz",
     output:
@@ -47,7 +47,7 @@ rule decompositions_long:
         count=config["decomposition"]["count"],
     shell:
         """
-        scripts/decompositions_long.sh \
+        scripts/decomposition_long.sh \
             -t {input.tc} \
             -l {params.length} \
             -c {params.count} \
--- scripts/decomposition_short.sh
+++ scripts/decomposition_short.sh
@@ -18,7 +18,7 @@ EOF
   exit 1;
 }
 
-while getopts b:t:g:c:l:n:s:o: opt; do
+while getopts b:t:g:c:l:n:C:s:o: opt; do
   case ${opt} in
   b) bw=${OPTARG};;
   t) tc=${OPTARG};;
@@ -50,67 +50,7 @@ awk -v length=${length} -v count=${count} '{
     if((($3 - $2) <= length) || ($5 <= count)){print}
 }' | \
 grep -- $signstr | \
-awk -v bw=${bw} -v color=${color} -v noise_subtraction_ratio=${noise} \
-'BEGIN{OFS="\t"}{
-
-  chrom = $1; start = $2; stop = $3; name = $4; score = $5; strand = $6;
-
-  ### for representative position ###
-  rep_start = 0; rep_stop = 0; rep_max = 0;
-  command = sprintf("bigWigToBedGraph -chrom=%s -start=%s -end=%s %s /dev/stdout",
-  chrom, start, stop, bw )
-  while ((command | getline ) > 0)
-  {
-    #total = total + ($4 * ($3 - $2))
-    if ($4 > rep_max){
-      rep_start = $2
-      rep_stop  = $3
-      rep_max   = $4
-    }
-  }
-  close(command)
-
-  ### for boundary trimming by noise subtraction ###
-  boundary_start = 0; boundary_stop = 0 ;
-  command = sprintf("bigWigToBedGraph -chrom=%s -start=%s -end=%s %s /dev/stdout | sort -k1,1 -k2,2n ",
-  chrom, start, stop, bw )
-  while ((command | getline ) > 0)
-  {
-    if ( ( $4 - (rep_max * noise_subtraction_ratio) ) > 0  ){
-      boundary_start = $2
-      break
-    }
-  }
-  close(command)
-
-  command = sprintf("bigWigToBedGraph -chrom=%s -start=%s -end=%s %s /dev/stdout | sort -k1,1 -k2,2nr ",
-  chrom, start, stop, bw )
-  while ((command | getline ) > 0)
-  {
-    if ( ( $4 - (rep_max * noise_subtraction_ratio) ) > 0  ){
-      boundary_stop = $3
-      break
-    }
-  }
-  close(command)
-
-  ### get total counts on the new boundary ###
-  total = 0
-  command = sprintf("bigWigToBedGraph -chrom=%s -start=%s -end=%s %s /dev/stdout",
-  chrom, boundary_start, boundary_stop, bw )
-  while ((command | getline ) > 0)
-  {
-    total = total + ($4 * ($3 - $2))
-  }
-  close(command)
-
-  if (total > 0)
-  {
-    # added this line at 13th Jan, 2012
-    name=chrom":"boundary_start".."boundary_stop","strand
-
-    print chrom, boundary_start, boundary_stop, name, total, strand,
-          rep_start, rep_stop, color
-  }
-}' | \
-> ${output}
+scripts/bed2peakBed9_with_boundaryTrimming.sh \
+    -r ${noise} \
+    -b ${bw} \
+    -c ${color} > ${output}
